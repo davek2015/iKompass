f36e4b1eb54fa294d1d926b4770368419687fb6a
DK Standard <DK@Daves-MacBook-Pro.local>
2015-07-19 11:32:05 +0800

    Initial Commit

---
+++ Day3/Day4/MyMovies/MyMovies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMovies
+//
+//  Created by DK Standard on 19/7/15.
+//  Copyright (c) 2015 iKompass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
--- Day3/Day4/TicTacToe/TicTacToe/ViewController.m
+++ Day3/Day4/TicTacToe/TicTacToe/ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *labelOne;
+@property (weak, nonatomic) IBOutlet UILabel *labelTwo;
+@property (weak, nonatomic) IBOutlet UILabel *labelThree;
+@property (weak, nonatomic) IBOutlet UILabel *labelFour;
+@property (weak, nonatomic) IBOutlet UILabel *labelFive;
+@property (weak, nonatomic) IBOutlet UILabel *labelSix;
+@property (weak, nonatomic) IBOutlet UILabel *labelSeven;
+@property (weak, nonatomic) IBOutlet UILabel *labelEight;
+@property (weak, nonatomic) IBOutlet UILabel *labelNine;
 
 @end
 
@@ -22,6 +32,69 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)findLabelUsingPoint: (CGPoint)point{
+    
+}
+
+-(IBAction)onLabelTap:(UITapGestureRecognizer *)sender{
+    
+    CGPoint point = [sender locationInView:self.view];
+    NSString *tmpString = self.whichPlayerLabel.text;
+    
+    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+                self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+
+    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    else if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    else if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    if (CGRectContainsPoint(self.labelOne.frame, point)){
+        if ([self.labelOne.text isEqualToString:@"?"]){
+            self.labelOne.text = self.whichPlayerLabel.text;
+        }
+    }
+    
+    
 }
 
 @end
